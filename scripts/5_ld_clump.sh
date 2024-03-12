c4c0a6caa99b888c6ce4d8a99146cd04efbf13a7
roskamsh <breesh.roskamshieter@gmail.com>
2024-03-12 13:34:37 +0000

    LD clumping a set of input SNPs using PLINK

---
+++ scripts/5_ld_clump.sh
@@ -0,0 +1,5 @@
+# Load: Modules and Environments
+module load igmm/apps/plink/1.90b4
+
+# Command
+plink --bfile output/bed_files/GenOMICC_ISARIC_info_score_0.6_chr6 --clump input/ESR1_cis_eQTLs_hg38_formatted_IDs.assoc --clump-p1 5e-6 --clump-p2 0.05 --clump-r2 0.2 --out output/clumps/ESR1_clumped_r0.2
