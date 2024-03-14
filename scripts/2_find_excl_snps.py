c167486eae2dd7b9ee0b373cc9003b175b6bf40b
roskamsh <breesh.roskamshieter@gmail.com>
2024-03-14 11:17:10 +0000

    cleanup

--- scripts/2_find_excl_snps.py
+++ scripts/2_find_excl_snps.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 
 df = pd.read_csv("output/chr6_imputation_stats.snpstats", delimiter="\t", skiprows=9)
@@ -8,7 +7,6 @@ snps = df[["alternate_ids","rsid","chromosome","position","alleleA","alleleB"]]
 snps.columns = ["SNPID","rsid","chromosome","position","alleleA","alleleB"] 
 
 rsids = " ".join(snps.rsid.values)
-snps.to_csv("output/chr6_variants2exclude_info_score0.6.txt", index = False, sep="\t")
 
 # Write rsids
 with open("output/chr6_rsids2exclude_info_score0.6.txt", "w") as text_file:
