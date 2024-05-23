80b320b15aaeae6eeaf7d3f84666fe2aa55cb6af
roskamsh <breesh.roskamshieter@gmail.com>
2024-05-23 14:51:13 +0100

    update input file

--- scripts/5_ld_clump.sh
+++ scripts/5_ld_clump.sh
@@ -2,4 +2,4 @@
 module load igmm/apps/plink/1.90b4
 
 # Command
-plink --bfile output/bed_files/GenOMICC_ISARIC_info_score_0.6_chr6 --clump input/ESR1_cis_eQTLs_hg38_formatted_IDs.assoc --clump-p1 5e-6 --clump-p2 0.05 --clump-r2 0.2 --out output/clumps/ESR1_clumped_r0.2
+plink --bfile output/bed_files/GenOMICC_ISARIC_info_score_0.6_chr6 --clump input/ESR1_ciseQTLs_hg38.assoc --clump-p1 5e-6 --clump-p2 0.05 --clump-r2 0.2 --out output/clumps/ESR1_clumped_r0.2
