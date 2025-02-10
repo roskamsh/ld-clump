25d75bf6dee46d920aae3b14e87f2ccefb042f94
roskamsh <breesh.roskamshieter@gmail.com>
2025-02-10 09:39:22 +0000

    update local script for YAML creation

--- scripts/create_estimands_file.py
+++ scripts/create_estimands_file.py
@@ -13,6 +13,7 @@ yaml.add_representer(QuotedString, quoted_presenter)
 
 # Function to convert all strings in a dictionary to a QuotedString
 def quote_strings(obj):
+    """Recursively convert strings in a dictionary or list to QuotedString."""
     if isinstance(obj, str):
         return QuotedString(obj)
     elif isinstance(obj, list):
@@ -21,11 +22,17 @@ def quote_strings(obj):
         return {key: quote_strings(value) for key, value in obj.items()}
     return obj
 
+# Custom Dumper to ensure correct indentation
+class IndentedDumper(yaml.Dumper):
+    """Add indentation where necessary in output YAML."""
+    def increase_indent(self, flow=False, indentless=False):
+        return super(IndentedDumper, self).increase_indent(flow, False)
+
 # TARGENE specifications
 outcome_extra_covariates = ["Age-Assessment","Genetic-Sex"]
 extra_treatments = []
 estimands_orders = [2,3]
-estimands_type = "AIE"
+estimands_type = ["AIE"]
 estimands_configuration_type = "groups"
 
 # Final SNPs
@@ -38,35 +45,24 @@ bqtls = bqtls[bqtls['TF'].isin(transactors.TF.unique())]
 bqtl_dictionary = {key: bqtls.loc[bqtls['TF'] == key, 'SNP'].tolist() for key in bqtls['TF']}
 transactors_dictionary = {key: transactors.loc[transactors['TF'] == key, 'SNP'].tolist() for key in transactors['TF']}
 
-joined = {}
+joined = {key: {'bQTLs': bqtl_dictionary.get(key, []), 'transActors': transactors_dictionary.get(key, [])} for key in bqtl_dictionary}
 
-for key in bqtl_dictionary:
-    joined[key] = {'bQTLs': bqtl_dictionary[key], 'transActors': transactors_dictionary[key]}
+# Construct the YAML data
+estimands_entry = {'orders': estimands_orders, 'type': estimands_type}
 
-# Generate complete dictionary
-if len(extra_treatments) == 0:
-    data = {
-        'type': estimands_configuration_type,
-        'estimands': [
-            {'type': estimands_type, 'orders': estimands_orders}
-        ],
-        'outcome_extra_covariates': outcome_extra_covariates,
-        'variants': joined
-    }
-else:
-    data = {
-        'type': estimands_configuration_type,
-        'estimands': [
-            {'type': estimands_type, 'orders': estimands_orders}
-        ],
-        'outcome_extra_covariates': outcome_extra_covariates,
-        'extra_treatments' : extra_treatments,
-        'variants': joined
-    } 
+data = {
+    'type': estimands_configuration_type,
+    'estimands': [estimands_entry],
+    'outcome_extra_covariates': outcome_extra_covariates,
+    'variants': joined
+}
+
+if extra_treatments:
+    data['extra_treatments'] = extra_treatments
 
 # Ensure quoted strings
 quoted_data = quote_strings(data)
 
-# Write to YAML
+# Write to YAML with correct indentation
 with open('estimands.yaml', 'w') as file:
-    yaml.dump(quoted_data, file, default_flow_style=False, sort_keys=False)
+    yaml.dump(quoted_data, file, default_flow_style=False, sort_keys=False, width=100, indent=2, Dumper=IndentedDumper)
